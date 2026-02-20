9a8722e98192db2a4aa11ba2bafa0dd904f70de1
Anna Benýšková <anna.benyskova@gmail.com>
2026-02-20 10:45:58 +0100

    change profil run

--- RUN.sh
+++ RUN.sh
@@ -2,4 +2,4 @@
 # . activate nextflow
 
 #nextflow run /mnt/hdd2/anna/Myelo/src/project/annbeny/Myelo-Discovery -C nextflow.config -profile standard -resume -with-report
-nextflow -C /mnt/hdd2/anna/Myelo/src/nextflow.config run /mnt/hdd2/anna/Myelo/src/project/annbeny/Myelo-Discovery -profile standard -resume -with-report
+nextflow -C /mnt/hdd2/anna/Myelo/src/nextflow.config run /mnt/hdd2/anna/Myelo/src/project/annbeny/Myelo-Discovery -profile high_mem -resume -with-report
